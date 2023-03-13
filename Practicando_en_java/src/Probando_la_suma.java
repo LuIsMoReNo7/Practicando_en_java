c92af493a63327eda271b693d31683e43da422ed
LuIsMoReNo7 <1483853@senati.pe>
2023-03-12 20:14:54 -0500

    usamos int y tambien hacemos una suma con los mismos valores en una linea

--- Practicando_en_java/src/Probando_la_suma.java
+++ Practicando_en_java/src/Probando_la_suma.java
@@ -1,7 +1,7 @@
 public class Probando_la_suma {
     public static void main(String[] args) {
-        int x = 5;
-        int y = 6;
+        int x = 6;
+        int y = 20;
         System.out.println(x + y); // imprimimos el valor de  x + y
       }
 }
---
+++ Practicando_en_java/src/Suma_de_tres_valores.java
@@ -0,0 +1,8 @@
+public class Suma_de_tres_valores {
+    public static void main(String[] args) {
+        int x = 4;
+        int y = 7;
+        int z = 10;
+        System.out.println(x + y + z);
+    }
+}
