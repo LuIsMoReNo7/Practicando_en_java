5da96148ed3f74032cc127a55d0164b320830b96
LuIsMoReNo7 <1483853@senati.pe>
2023-03-12 19:50:05 -0500

    probando con last name

--- Practicando_en_java/src/Combinacion_de_texto.java
+++ Practicando_en_java/src/Combinacion_de_texto.java
@@ -1,6 +1,8 @@
 public class Combinacion_de_texto {
     public static void main(String[] args) {
         String name = " Jhon ";
-        System.out.println("Hello" + name);
+        String lastName = "Doe";
+        String fullName = name + lastName;
+        System.out.println(fullName);
     }
 }
