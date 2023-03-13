7f940bd4a15893df5a6e76dacb7cbaa99399ad40
LuIsMoReNo7 <1483853@senati.pe>
2023-03-12 20:26:53 -0500

    multiplicacion con valores predeterminados

---
+++ Practicando_en_java/src/multiplicacion_poniendo_valores_predeterminados.java
@@ -0,0 +1,7 @@
+public class multiplicacion_poniendo_valores_predeterminados {
+    public static void main(String[] args) {
+        int x = 5;
+        int y = 7;
+        System.out.println(x * y);
+    }
+}
